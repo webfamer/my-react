e81b2e20209a241cb7d6c265a2332ae4f666a036
xsz <webfamer@163.com>
2023-03-03 15:35:20 +0800

    feature(reconciler):完成调和(组件更新对比)过程

--- src/index.js
+++ src/index.js
@@ -17,32 +17,89 @@ function createTextElement(text) {
     }
 }
 let nextUnitOfWork = null
-
+let wipRoot = null
+let currentRoot = null //更新前的根节点fiber树
+let delections = null //存储要删除的fiber
 function render(element, container) {
     //将根节点设置成第一个要完成的工作单元
-    nextUnitOfWork = {
+    wipRoot = {
         dom: container,
         props: {
             children: [element]
-        }
+        },
+        alternate: currentRoot
     }
+    nextUnitOfWork = wipRoot
+    delections = []
 }
-function createDom(element, container) {
-    const dom = element.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(element.type)
-    const isProperty = prop => prop != 'children'
-    Object.keys(element.props).filter(isProperty).forEach(item => {
-        dom[item] = element.props[item]
-    })
+function createDom(fiber) {
+    const dom = fiber.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(element.type)
+    updateDom(dom, {}, fiber.props)
     return dom
 }
+const isProperty = key => key !== "children" &&!isEvent(key)
+const isNew = (prev, next) => key => prev[key] !== next[key] //判断是否有新属性
+const isGone = (prev, next) => key => !(key in next) //是否是旧属性
+const isEvent = key =>key.startsWith('on')
+function updateDom(dom, prevProps, nextProps) {
+    //移除老的属性
+    Object.keys(prevProps).filter(isProperty).filter(isGone(prevProps, nextProps)).forEach(name => dom[name] = '')
+   //移除老的事件监听
+   Object.keys(prevProps)
+   .filter(isEvent).filter(key =>!(key in nextProps)||isNew(prevProps,nextProps)(key))
+   .forEach(name=>{
+    const eventType = name.toLowerCase().substring(2)
+    dom.removeEventListener(eventType,prevProps[name])
+   })
+    //设置新的属性
+    Object.keys(nextProps).filter(isProperty).filter(isNew(prevProps, nextProps)).forEach(name => { dom[name] = nextProps[name] })
+   //添加新的事件处理
+   Object.keys(nextProps).filter(isEvent).filter(isNew(prevProps, nextProps)).forEach(name =>{
+    const eventType = name.toLowerCase().substring(2)
+    dom.addEventListener(eventType,nextProps[name])
+   })
+}
 
+//递归处理fiber tree
+function commitWork(fiber) {
+    if (!fiber) {
+        return
+    }
+    const domParent = fiber.parent.dom
+    if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
+        domParent.appendChild(fiber.dom)
+    } else if (fiber.effectTag === 'DELETION') {
+        domParent.removeChild(fiber.dom)
+    } else if (
+        fiber.effectTag === "UPDATE" && fiber.dom != null
+    ) {
+        updateDom(
+            fiber.dom,
+            fiber.alternate.props,
+            fiber.props
+        )
+    }
+    //渲染子节点
+    commitWork(fiber.child)
+    //渲染兄弟节点
+    commitWork(fiber.sibling)
+}
+//将fiber tree渲染为真实DOM
+function commitRoot() {
+    delections.forEach(commitWork)
+    commitWork(wipRoot.child)
+    currentRoot = wipRoot
+    wipRoot = null
+}
 function workLoop(deadline) {
     let shouldYield = false
     while (nextUnitOfWork && !shouldYield) {
         nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
         //当前帧空余时间要没了，停止工作循环
         shouldYield = deadline.timeRemaining() < 1
-        console.log(shouldYield,'shouldYield')
+    }
+    if (!nextUnitOfWork && wipRoot) {
+        commitRoot()
     }
     //空闲时间执行任务
     requestIdleCallback(workLoop)
@@ -50,45 +107,69 @@ function workLoop(deadline) {
 requestIdleCallback(workLoop)
 
 function performUnitOfWork(fiber) {
-   if(!fiber.dom){
-    fiber.dom = createDom(fiber)
-   }
-   if(fiber.parent){
-    fiber.parent.dom.appendChild(fiber.dom)
-   }
-   //获取当前fiber的孩子节点
-   const elements = fiber.props.children
-   let index = 0
-   let prevSibling = null
-
-   while(index <elements.length){
-    const element = elements[index]
-    const newFiber = {
-        type:element.type,
-        props:element.props,
-        parent:fiber,
-        dom:null
+    if (!fiber.dom) {
+        fiber.dom = createDom(fiber)
     }
-    if(index ===0){
-        fiber.child = newFiber
-    }else if(element){
-        prevSibling.sibling = newFiber
+    //获取当前fiber的孩子节点
+    const elements = fiber.props.children
+    reconcileChildren(fiber, elements)
+    if (fiber.child) {
+        return fiber.child
     }
-    prevSibling = newFiber;
-    index ++
-   }
-   if(fiber.child){
-    return fiber.child
-   }
-   let nextFiber = fiber
-   while(nextFiber){
-    if(nextFiber.sibling){
-        return nextFiber.sibling
+    let nextFiber = fiber
+    while (nextFiber) {
+        if (nextFiber.sibling) {
+            return nextFiber.sibling
+        }
+        nextFiber = nextFiber.parent
     }
-    nextFiber = nextFiber.parent
-   }
 }
 
+function reconcileChildren(wipFiber, elements) {
+    let index = 0
+    let prevSibling = null
+    //上一次渲染的fiber
+    let oldFiber = wipFiber.alternate && wipFiber.alternate.child
+    while (index < elements.length || oldFiber != null) {
+        const element = elements[index]
+        let newFiber = null
+        const sameType = oldFiber && element && element.type == oldFiber.type
+        if (sameType) {
+            newFiber = {
+                type: oldFiber.type,
+                props: element.props,
+                dom: oldFiber.dom,
+                parent: wipFiber,
+                alternate: oldFiber,
+                effectTag: "UPDATE",
+            }
+        }
+        if (element && !sameType) {
+            newFiber = {
+                type: element.type,
+                props: element.props,
+                dom: null,
+                parent: wipFiber,
+                alternate: null,
+                effectTag: "PLACEMENT"
+            }
+        }
+        if (oldFiber && !sameType) {
+            oldFiber.effectTag = "DELETION"
+            delections.push(oldFiber)
+        }
+        if(oldFiber){
+            oldFiber = oldFiber.sibling
+        }
+        if (index === 0) {
+            wipFiber.child = newFiber
+        } else if (element) {
+            prevSibling.sibling = newFiber
+        }
+        prevSibling = newFiber;
+        index++
+    }
+}
 
 const Didact = {
     createElement,
@@ -98,6 +179,7 @@ const Didact = {
 const element = (
     <div style="background: salmon">
         <h1>Hello World</h1>
+        <input type="text"/>
         <h2 style="text-align:right">from1 Didact</h2>
     </div>
 );
